2ae2755bc641d355371d4840354818098e43fbb8
yusukesugahara <yusuke.sugahara@gmail.com>
2026-06-05 22:44:32 +0900

    feat(frontend): update TooltipContent component to include children and an arrow for improved visibility

--- apps/frontend/src/components/ui/tooltip.tsx
+++ apps/frontend/src/components/ui/tooltip.tsx
@@ -14,7 +14,7 @@ const TooltipTrigger = TooltipPrimitive.Trigger;
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
+>(({ children, className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Portal>
     <TooltipPrimitive.Content
       ref={ref}
@@ -24,7 +24,10 @@ const TooltipContent = React.forwardRef<
         className,
       )}
       {...props}
-    />
+    >
+      {children}
+      <TooltipPrimitive.Arrow className="fill-slate-900" />
+    </TooltipPrimitive.Content>
   </TooltipPrimitive.Portal>
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
